76f13f26a5168e224aaeea235392d231a8bef812
Ali Motevallian <alireza.motevallian@gmail.com>
2017-07-27 03:58:44 +1000

    Retrieving data from the API, converting it to the expected list and showing it in the UI.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
-import {CatsList} from './models/person.model';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/share';
+import 'rxjs/add/operator/do';
+
+
+import {CatsList, Person} from './models/person.model';
+import {LoadPeopleService} from './people.service/load-people.service';
+import {CatsExtractorService} from './cats.service/cats-extractor.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public maleCatsList: Observable<string[]>;
+  public femaleCatsList: Observable<string[]>;
 
-  public catsList: CatsList = {Male: ['Garfield', 'Jim', 'Max', 'Tom'], Female: ['Garfield', 'Simba', 'Tabby']};
-
-  constructor() {
+  constructor(private loadPeople: LoadPeopleService, private catExtraxtor: CatsExtractorService) {
 
   }
+
+  ngOnInit(): void {
+    const people$ = this.loadPeople.retrievePeople()
+      .map((people: Person[]) => {
+        return this.catExtraxtor.extractCats(people);
+      }).share()
+      .do((x) => {
+          console.log('value retrieved', x);
+        });
+
+    this.maleCatsList = people$.map((catsList: CatsList) => catsList.Male);
+    this.femaleCatsList = people$.map((catsList: CatsList) => catsList.Female);
+  }
+
 }
